6b67f48b68110c049511e7a9726f09e8f0298ff1
fuyou <fuyou_work@hotmail.com>
2023-11-26 20:42:44 +0800

    Home部分完成

--- vue-rabbit/src/main.js
+++ vue-rabbit/src/main.js
@@ -7,10 +7,13 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// 引入懒加载指令插件并且注册
+import { lazyPlugin } from "@/directives";
+
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(lazyPlugin)
 app.mount('#app')
--- vue-rabbit/src/views/Home/index.vue
+++ vue-rabbit/src/views/Home/index.vue
@@ -1,4 +1,27 @@
+<script setup>
+import HomeCategory from './components/HomeCategory.vue'
+import HomeBanner from './components/HomeBanner.vue'
+import HomeNew from './components/HomeNew.vue'
+import HomeHot from './components/HomeHot.vue'
+import HomeProduct from './components/HomeProduct.vue'
+import HomePanel from './components/HomePanel.vue'
+</script>
+
 <template>
-  <div>我是home页面</div>
-  <div style="height: 1500px;"></div>
+  <div class="container">
+    <HomeCategory />
+    <HomeBanner />
+  </div>
+  <HomeNew />
+  <HomeHot />
+  <HomeProduct />
+
+  <!-- 测试面板组件 -->
+  <HomePanel title="新鲜好物" sub-title="新鲜好物 好多商品">
+    <div>11111111</div>
+  </HomePanel>
+
+  <HomePanel title="人气推荐" sub-title="人气推荐 好多商品">
+    <div>11111111</div>
+  </HomePanel>
 </template>
