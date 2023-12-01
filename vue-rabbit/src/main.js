50c6283f5088e839dbe54ba616cb79d79fe66977
fuyou <fuyou_work@hotmail.com>
2023-12-01 21:22:14 +0800

    详细页部分完成

--- vue-rabbit/src/main.js
+++ vue-rabbit/src/main.js
@@ -8,7 +8,9 @@ import App from './App.vue'
 import router from './router'
 
 // 引入懒加载指令插件并且注册
-import { lazyPlugin } from "@/directives";
+import { lazyPlugin } from "@/directives"
+// 引入全局组件插件
+import { componentPlugin } from '@/components'
 
 
 const app = createApp(App)
@@ -16,4 +18,5 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(lazyPlugin)
+app.use(componentPlugin)
 app.mount('#app')
--- vue-rabbit/src/views/Detail/index.vue
+++ vue-rabbit/src/views/Detail/index.vue
@@ -1,6 +1,5 @@
 <script setup>
 import DetailHot from './components/DetailHot.vue'
-import ImageView from '@/components/ImageView/index.vue'
 import { getDetail } from '@/apis/detail'
 import { onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
@@ -15,6 +14,11 @@ const getGoods = async () =>
 
 onMounted(() => getGoods())
 
+// sku规格被操作时
+const skuChange = (sku) =>
+{
+  console.log(sku)
+}
 </script>
 
 <template>
@@ -43,7 +47,7 @@ onMounted(() => getGoods())
           <div class="goods-info">
             <div class="media">
               <!-- 图片预览区 -->
-              <ImageView />
+              <XtxImageView :image-list="goods.mainPictures" />
               <!-- 统计数量 -->
               <ul class="goods-sales">
                 <li>
@@ -92,7 +96,7 @@ onMounted(() => getGoods())
                 </dl>
               </div>
               <!-- sku组件 -->
-
+              <XtxSku :goods="goods" @change="skuChange" />
               <!-- 数据组件 -->
 
               <!-- 按钮组件 -->
