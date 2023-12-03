af537d9f06e0e8b9ba9dc594cabffbf12221d364
fuyou <fuyou_work@hotmail.com>
2023-12-03 18:17:40 +0800

    登录部分完成

--- vue-rabbit/src/utils/http.js
+++ vue-rabbit/src/utils/http.js
@@ -1,5 +1,9 @@
 // axios基础的封装
 import axios from 'axios'
+import 'element-plus/es/components/message/style/css'
+import { ElMessage } from 'element-plus'
+import { useUserStore } from '@/stores/user'
+import router from '@/router'
 
 // 创建axios实例
 const httpInstance = axios.create({
@@ -10,12 +14,36 @@ const httpInstance = axios.create({
 // axios请求拦截器
 httpInstance.interceptors.request.use(config =>
 {
+    // 1. 从pinia获取token数据
+    const userStore = useUserStore()
+    // 2. 按照后端的要求拼接token数据
+    const token = userStore.userInfo.token
+    if (token)
+    {
+        config.headers.Authorization = `Bearer ${token}`
+    }
     return config
 }, e => Promise.reject(e))
 
 // axios响应式拦截器
 httpInstance.interceptors.response.use(res => res.data, e =>
 {
+    const userStore = useUserStore()
+    // 统一错误提示
+    ElMessage({
+        type: 'warning',
+        message: e.response.data.msg
+    })
+
+    // 401token失效处理
+    // 1. 清除本地用户数据
+    // 2. 跳转到登录页
+    if (e.response.status === 401)
+    {
+        userStore.clearUsrInfo()
+        router.push('/login')
+    }
+
     return Promise.reject(e)
 })
 
