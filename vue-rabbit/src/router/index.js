4f1f81da93b82fc41217d80eeeabecd69d048ce2
fuyou <fuyou_work@hotmail.com>
2023-12-04 14:33:11 +0800

    购物车部分 - 本地列表购物车基础数据渲染

--- vue-rabbit/src/router/index.js
+++ vue-rabbit/src/router/index.js
@@ -8,6 +8,7 @@ import Home from '@/views/Home/index.vue'
 import Category from '@/views/Category/index.vue'
 import subCategory from '@/views/subCategory/index.vue'
 import Detail from '@/views/Detail/index.vue'
+import CartList from '@/views/CartList/index.vue'
 
 
 const router = createRouter({
@@ -33,6 +34,10 @@ const router = createRouter({
 				{
 					path: 'detail/:id',
 					component: Detail
+				},
+				{
+					path: 'cartlist',
+					component: CartList
 				}
 			]
 		},
--- vue-rabbit/src/views/Detail/index.vue
+++ vue-rabbit/src/views/Detail/index.vue
@@ -1,9 +1,12 @@
 <script setup>
 import DetailHot from './components/DetailHot.vue'
 import { getDetail } from '@/apis/detail'
+import { ElMessage } from 'element-plus';
 import { onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
+import { useCartStore } from '@/stores/cartStore'
 
+const cartStore = useCartStore()
 const goods = ref({})
 const route = useRoute()
 const getGoods = async () =>
@@ -15,10 +18,44 @@ const getGoods = async () =>
 onMounted(() => getGoods())
 
 // sku规格被操作时
+let skuObj = {}
 const skuChange = (sku) =>
 {
-  console.log(sku)
+  // console.log(sku)
+  skuObj = sku
 }
+
+// count
+const count = ref(1)
+const countChange = () =>
+{
+  // console.log(count)
+}
+
+// 添加购物车
+const addCart = () =>
+{
+  if (skuObj.skuId)
+  {
+    // 规则已经选择 触发action
+    cartStore.addCart({
+      id: goods.value.id,
+      name: goods.value.name,
+      picture: goods.value.mainPictures[0],
+      price: goods.value.price,
+      count: count.value,
+      skuId: skuObj.skuId,
+      attrsText: skuObj.specsText,
+      selected: true
+    })
+  } else
+  {
+    // 规则没有选择 提示用户
+    ElMessage.warning('请选择规格')
+  }
+}
+
+
 </script>
 
 <template>
@@ -98,10 +135,10 @@ const skuChange = (sku) =>
               <!-- sku组件 -->
               <XtxSku :goods="goods" @change="skuChange" />
               <!-- 数据组件 -->
-
+              <el-input-number v-model="count" @change="countChange" />
               <!-- 按钮组件 -->
               <div>
-                <el-button size="large" class="btn">
+                <el-button size="large" class="btn" @click="addCart">
                   加入购物车
                 </el-button>
               </div>
